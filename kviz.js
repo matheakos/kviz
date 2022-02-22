11c70558b315a36f7d029b7396ec0d07c238df14
Mathe Akos <matheakos2002@gmail.com>
2022-02-22 13:18:58 +0100

    ellenorzescommit

--- kviz.js
+++ kviz.js
@@ -1,5 +1,6 @@
 var radiogomb;
 var select;
+var marvel;
 var kekes;
 var film;
 
@@ -19,14 +20,40 @@ function masodikfeladat(){
     console.log(masodik.options[masodik.selectedIndex].value);
 }
 
+function harmadikfeladat(){
+    var harmadik = document.getElementById("harmadik").value;
+    marvel = harmadik;
+    console.log(marvel);
+}
+
 function negyedikfeladat(){
-    var negyedik = document.getElementById("negyedik");
+    var negyedik = document.getElementById("negyedik").value;
     kekes = negyedik;
     console.log(kekes);
 }
 
 function otodikfeladat(){
-    var otodik = documnet.getElementById("otodik");
+    var otodik = document.getElementById("otodik").value;
     film = otodik;
     console.log(film);
+}
+
+var pontszam = 0;
+function ellenorzogomb(){
+    if(radiogomb == "Naughty Dog"){
+        pontszam += 1;
+    }
+    if(select == "Socket AM4"){
+        pontszam += 1;
+    }
+    if(marvel == "A"){
+        pontszam += 1;
+    }
+    if(kekes == "Mátra"){
+        pontszam += 1;
+    }
+    if(film == "Guy Ritchie"){
+        pontszam += 1;
+    }
+    console.log(pontszam);
 }
